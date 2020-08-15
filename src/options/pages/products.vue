a2f1f8a6dd8ee60e1c2264d71464e76c467309c8
giangmkqn <giangmkios@gmail.com>
2020-08-15 15:34:40 +0700

    giangnt commit test

--- src/options/pages/products.vue
+++ src/options/pages/products.vue
@@ -1,15 +1,39 @@
 <template>
-    <h1>product</h1>
+  <v-row>
+    <v-col cols="6" md>
+      <h1 class>product</h1>
+    </v-col>
+    <v-col cols="6" md>
+      <h1>{{ countString }}</h1>
+      <button class @click="increase()">Increase</button>
+      <button class @click="decrease()">Decrease</button>
+    </v-col>
+  </v-row>
 </template>
-
 <script>
 export default {
-    data: () => {
-        return {};
-    }
-}
+  data: () => {
+    return {
+      counter: 1,
+    };
+  },
+  created: () => {},
+  methods: {
+    increase: function() {
+      this.counter++;
+    },
+    decrease: function() {
+      this.counter--;
+    },
+  },
+  computed: {
+    countString() {
+      return this.counter + 'k';
+    },
+  },
+  components: {},
+  props: {},
+};
 </script>
 
-<style scoped>
-
-</style>
+<style lang="scss" scoped></style>

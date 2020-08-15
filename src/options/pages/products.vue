a26b829d546b50d7b681aa4b735e3c70f842047d
giangmkqn <giangmkios@gmail.com>
2020-08-15 15:38:26 +0700

    fix commit test

--- src/options/pages/products.vue
+++ src/options/pages/products.vue
@@ -1,7 +1,10 @@
 <template>
   <v-row>
-    <v-col cols="6" md>
+    <v-col cols="3" md>
       <h1 class>product</h1>
+    </v-col>
+    <v-col cols="3" md>
+      <h1 class>1</h1>
     </v-col>
     <v-col cols="6" md>
       <h1>{{ countString }}</h1>

46f74e3aeca5bed974a0f70aa88b5c92794435cf
DESKTOP-3F72DP2\My computer <giangmkios@gmail.com>
2020-08-29 02:13:31 +0700

    event create update order

--- src/options/pages/orders.vue
+++ src/options/pages/orders.vue
@@ -2,8 +2,8 @@
   <v-row>
     <v-col cols="12">
       <h1>
-        Thêm mới sản phẩm
-        <v-btn class="mx-2" fab small dark color="success">
+        Thêm mới order
+        <v-btn class="mx-2" fab small dark color="success" @click="openCreate()">
           <v-icon dark>mdi-plus</v-icon>
         </v-btn>
       </h1>
@@ -29,14 +29,20 @@
               <td>{{ item.full_name }}</td>
               <td>{{ item.phone }}</td>
               <td>{{ item.address }}</td>
-              <td>{{ item.cod }}</td>
-              <td>{{ item.value }}</td>
-              <td>{{ item.status }}</td>
+              <td>
+                <v-chip :color="cod(item.cod).color" x-small>
+                  {{ cod(item.cod).text }}
+                </v-chip>
+              </td>
+              <td>{{ formatCurrency(item.value) }}</td>
+              <td>
+                {{ item.status }}
+              </td>
               <td>
                 <v-btn icon color="primary" title="Tạo mới đơn hàng">
                   <v-icon>mdi-plus</v-icon>
                 </v-btn>
-                <v-btn icon color="success" title="Chỉnh sửa đơn hàng">
+                <v-btn icon color="success" @click="openUpdate(item)" title="Chỉnh sửa đơn hàng">
                   <v-icon>mdi-pencil</v-icon>
                 </v-btn>
               </td>
@@ -53,11 +59,17 @@
       <div class="text-center mt-4">
         <v-pagination v-model="page" :length="3"></v-pagination>
       </div>
+      <!-- Modal add new product -->
+      <order-dialog ref="orderDialog" v-on:order="handleOrder"></order-dialog>
     </v-col>
   </v-row>
 </template>
 <script>
+import OrderDialog from './components/order/OrderDialog';
 export default {
+  components: {
+    OrderDialog,
+  },
   data: () => {
     return {
       loading: true,
@@ -66,6 +78,9 @@ export default {
     };
   },
   methods: {
+    formatCurrency(price) {
+      return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
+    },
     initialize() {
       fetch('https://my.api.mockaroo.com/data.json?key=060bc380').then(async response => {
         const result = await response.json();
@@ -74,21 +89,41 @@ export default {
         this.data = result;
       });
     },
+    openCreate() {
+      this.$refs.orderDialog.openCreate();
+    },
+    openUpdate(obj) {
+      this.$refs.orderDialog.openUpdate(obj);
+    },
+    handleOrder(data) {
+      this.$refs.orderDialog.reset();
+      this.$refs.orderDialog.close();
+      if (!data.response.ok) {
+        // this.alert('Thêm thất bại', 'rgb(176, 0, 32)');
+      }
+      var dataOrder = data.item;
+      if (dataOrder.id === 0) {
+        dataOrder.id = this.data[this.data.length - 1].id + 1;
+        this.data.push(dataOrder);
+      } else {
+        const index = this.data.findIndex(x => x.id === dataOrder.id);
+        this.data[index].full_name = dataOrder.full_name;
+        this.data[index].phone = dataOrder.phone;
+        this.data[index].address = dataOrder.address;
+        this.data[index].cod = dataOrder.cod;
+        this.data[index].value = dataOrder.value;
+        this.data[index].status = dataOrder.status;
+      }
+    },
+    cod: function(item) {
+      const color = item ? 'success' : 'warning';
+      const text = item ? 'COD' : 'Không';
+      return { color, text };
+    },
   },
+
   created() {
     this.initialize();
   },
 };
 </script>
-
-<style scoped>
-.v-application .headline {
-  font-size: 20px !important;
-}
-.v-progress-circular {
-  margin: 1rem;
-}
-.text-loading {
-  color: rgb(29, 28, 28);
-}
-</style>

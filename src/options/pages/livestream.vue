91693ba5d72f4011937bbbc45b24902f30e73bc1
Alex <boytunghc@gmail.com>
2020-08-31 16:23:10 +0700

    reduce code

--- src/options/pages/livestream.vue
+++ src/options/pages/livestream.vue
@@ -8,13 +8,13 @@
             <v-spacer></v-spacer>
             <v-text-field v-model="search" append-icon="mdi-magnify" label="Tìm kiếm" single-line hide-details></v-text-field>
           </v-card-title>
-          <v-data-table v-model="selected" :headers="headers" :search="search" :items="comments" item-key="_id" show-select class="elevation-1">
+          <v-data-table :headers="headers" :search="search" :items="comments" show-select class="elevation-1">
             <template v-slot:[`item.created_at`]="{ item }">
               <span> {{ new Date(item.created_at).toLocaleString() }}</span>
             </template>
             <template v-slot:[`item.actions`]="{ item }">
-              <v-btn icon color="success">
-                <v-icon>mdi-pencil</v-icon>
+              <v-btn @click="editComment(item._id)" small outlined depressed color="primary">
+                Lên đơn
               </v-btn>
               <v-btn icon color="#B00020">
                 <v-icon>mdi-trash-can-outline</v-icon>
@@ -23,15 +23,21 @@
           </v-data-table>
         </v-card>
       </template>
+
+      <create-order-dialog ref="createDialog"></create-order-dialog>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import CreateOrderDialog from './components/livestream/CreateOrderDialog';
+
 export default {
+  components: {
+    CreateOrderDialog,
+  },
   data() {
     return {
-      selected: [],
       search: '',
       headers: [
         {
@@ -42,10 +48,9 @@ export default {
         },
         { text: 'Comment', value: 'message' },
         { text: 'Ngày', value: 'created_at' },
-        { text: 'Hành động', value: 'actions', sortable: false, width: 120 },
+        { text: 'Hành động', value: 'actions', sortable: false, width: 170 },
       ],
       comments: [],
-      products: [],
     };
   },
   methods: {
@@ -53,22 +58,24 @@ export default {
       // eslint-disable-next-line no-undef
       app.Echo.channel(user._id).listen('CommentCreated', data => this.addComment(data.comment));
 
+      // eslint-disable-next-line prettier/prettier
       app
         .fetch('api/me/comments')
         .then(res => res.json())
         .then(body => {
           this.comments = body.data;
         });
-
-      app
-        .fetch('api/me/products?item=1000')
-        .then(res => res.json())
-        .then(body => {
-          this.products = body.data;
-        });
     },
     addComment(comment) {
       this.comments.push(comment);
+    },
+    editComment(_id) {
+      const index = this.findCommentIndex(_id);
+
+      this.$refs.createDialog.open(this.comments[index]);
+    },
+    findCommentIndex(id) {
+      return this.comments.map(x => x._id).indexOf(id);
     },
   },
   created() {

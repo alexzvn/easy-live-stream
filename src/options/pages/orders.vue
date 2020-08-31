1d93f03633ccd86825ad7ed8883bcdbe18ad1aa8
Alex <boytunghc@gmail.com>
2020-08-31 16:22:42 +0700

    reduce code orders

--- src/options/pages/orders.vue
+++ src/options/pages/orders.vue
@@ -3,7 +3,7 @@
     <v-col cols="12">
       <h1>
         Thêm mới order
-        <v-btn class="mx-2" fab small dark color="success" @click="openCreate()">
+        <v-btn class="mx-2" fab small dark color="success" @click="openCreateDialog">
           <v-icon dark>mdi-plus</v-icon>
         </v-btn>
       </h1>
@@ -13,36 +13,25 @@
         <template v-slot:default>
           <thead>
             <tr>
-              <th>Mã đơn</th>
+              <th>STT</th>
               <th class="text-left">Tên</th>
               <th class="text-left">SĐT</th>
               <th class="text-left">Địa chỉ</th>
-              <th class="text-left">COD</th>
               <th class="text-left">Giá trị</th>
               <th class="text-left">Trạng thái</th>
               <th class="text-left">Hành động</th>
             </tr>
           </thead>
           <tbody v-show="!loading">
-            <tr v-for="item in data" :key="item.id">
-              <td>{{ item.id }}</td>
-              <td>{{ item.full_name }}</td>
-              <td>{{ item.phone }}</td>
-              <td>{{ item.address }}</td>
+            <tr v-for="(order, index) in ordersMapped" :key="order.id">
+              <td>{{ index + 1 }}</td>
+              <td>{{ order.name }}</td>
+              <td>{{ order.phone }}</td>
+              <td>{{ order.address }}</td>
+              <td>{{ order.total_price }}</td>
+              <td>{{ order.status }}</td>
               <td>
-                <v-chip :color="cod(item.cod).color" x-small>
-                  {{ cod(item.cod).text }}
-                </v-chip>
-              </td>
-              <td>{{ formatCurrency(item.value) }}</td>
-              <td>
-                {{ item.status }}
-              </td>
-              <td>
-                <v-btn icon color="primary" title="Tạo mới đơn hàng">
-                  <v-icon>mdi-plus</v-icon>
-                </v-btn>
-                <v-btn icon color="success" @click="openUpdate(item)" title="Chỉnh sửa đơn hàng">
+                <v-btn icon color="success" title="Chỉnh sửa đơn hàng">
                   <v-icon>mdi-pencil</v-icon>
                 </v-btn>
               </td>
@@ -57,10 +46,11 @@
         </p>
       </div>
       <div class="text-center mt-4">
-        <v-pagination v-model="page" :length="3"></v-pagination>
+        <v-pagination v-on:input="viewPage" v-on:next="nextPage" v-on:previous="nextPage" v-model="pagination.current_page" :length="pagination.last_page" :total-visible="7">
+        </v-pagination>
       </div>
       <!-- Modal add new product -->
-      <order-dialog ref="orderDialog" v-on:order="handleOrder"></order-dialog>
+      <create-order-dialog ref="createDialog" v-on:created="orderCreated"></create-order-dialog>
       <!-- Snackbar -->
       <v-col class="snackbar-info" cols="12">
         <v-snackbar v-model="snackbar.visible" :color="snackbar.color" timeout="2000" top="top">
@@ -76,16 +66,23 @@
   </v-row>
 </template>
 <script>
-import OrderDialog from './components/order/OrderDialog';
+import CreateOrderDialog from './components/livestream/CreateOrderDialog';
+
 export default {
   components: {
-    OrderDialog,
+    CreateOrderDialog,
   },
   data: () => {
     return {
-      loading: true,
-      page: 1,
-      data: [],
+      loading: false,
+      orders: [],
+      pagination: {
+        current_page: 0,
+        from: 0,
+        last_page: 0,
+        per_page: 5,
+        to: 0,
+      },
       snackbar: {
         color: '',
         visible: false,
@@ -93,50 +90,58 @@ export default {
       },
     };
   },
-  methods: {
-    formatCurrency(price) {
-      return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
-    },
-    initialize() {
-      fetch('https://my.api.mockaroo.com/data.json?key=060bc380').then(async response => {
-        const result = await response.json();
-        if (!response.ok) return;
-        this.loading = false;
-        this.data = result;
+  computed: {
+    ordersMapped() {
+      const orderStatus = {
+        1: 'Đang chờ',
+        2: 'Đang xử lý',
+        3: 'Đang giao hàng',
+        4: 'Đã giao hàng',
+        5: 'Khách hàng hủy',
+      };
+
+      const formatPrice = price => Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
+
+      return this.orders.map(order => {
+        return {
+          ...order,
+          status: orderStatus[order.status],
+          price: formatPrice(order.price),
+          total_price: formatPrice(order.total_price),
+        };
       });
     },
-    openCreate() {
-      this.$refs.orderDialog.openCreate();
+  },
+  methods: {
+    openCreateDialog() {
+      this.$refs.createDialog.open();
     },
-    openUpdate(obj) {
-      this.$refs.orderDialog.openUpdate(obj);
-    },
-    handleOrder(data) {
-      this.$refs.orderDialog.reset();
-      this.$refs.orderDialog.close();
-      if (!data.response.ok) {
-        this.alert('Thêm thất bại', 'rgb(176, 0, 32)');
+    async orderCreated(response, order) {
+      if (!response.ok) {
+        return this.alert('Có lỗi trong quá trình tạo đơn hàng', 'danger');
       }
-      var dataOrder = data.item;
-      if (dataOrder.id === 0) {
-        dataOrder.id = this.data[this.data.length - 1].id + 1;
-        this.data.push(dataOrder);
-        this.alert('Thêm thành công');
-      } else {
-        const index = this.data.findIndex(x => x.id === dataOrder.id);
-        this.data[index].full_name = dataOrder.full_name;
-        this.data[index].phone = dataOrder.phone;
-        this.data[index].address = dataOrder.address;
-        this.data[index].cod = dataOrder.cod;
-        this.data[index].value = dataOrder.value;
-        this.data[index].status = dataOrder.status;
-        this.alert('Cập nhật thành công');
-      }
+
+      this.orders.unshift((await response.json()).data);
+      this.alert('Đã thêm đơn hàng mới');
+      this.$refs.createDialog.close();
     },
-    cod: function(item) {
-      const color = item ? 'success' : 'warning';
-      const text = item ? 'COD' : 'Không';
-      return { color, text };
+    nextPage() {
+      this.fetchOrders(this.pagination.current_page);
+    },
+    viewPage(num) {
+      this.fetchOrders(num);
+    },
+    previousPage() {
+      this.fetchOrders(this.pagination.current_page - 1);
+    },
+    fetchOrders(page = 1) {
+      app
+        .fetch(`/api/me/orders?item=${this.pagination.per_page}&page=${page}`)
+        .then(res => res.json())
+        .then(body => {
+          this.orders = body.data;
+          this.pagination = body.meta;
+        });
     },
     alert(text = '', color = 'success') {
       this.snackbar = {
@@ -148,7 +153,7 @@ export default {
   },
 
   created() {
-    this.initialize();
+    this.fetchOrders();
   },
 };
 </script>
